f329285a7c7fe2791c0a11f75e9dbe05deaa1348
hungryfire267 <87181326+hungryfire267@users.noreply.github.com>
2022-08-31 02:59:39 +1000

    Changed Interface

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 
+const body = document.querySelector('body')
+const button_1 = document.createElement('button'); 
+button_1.setAttribute('id', 'Rock'); 
+button_1.textContent = 'Rock';
+body.appendChild(button_1);
+
+const button_2 = document.createElement('button'); 
+button_2.setAttribute('id', 'Paper'); 
+button_2.textContent = 'Paper';
+body.appendChild(button_2);
+const button_3 = document.createElement('button'); 
+button_3.setAttribute('id', 'Scissors'); 
+button_3.textContent = 'Scissors';
+body.appendChild(button_3);
+
+
+
 new_list = ['Rock', 'Paper', 'Scissors'];
 function getComputerChoice() {
     return new_list[Math.floor(Math.random() * 3)]
 }
+
 
 function convert_selection_string() {
     let selection = prompt("Enter your selection");
@@ -22,39 +40,65 @@ function winner(computer_score, person_score) {
     }
 }
 
-function game() {
-    computer_score = 0;
-    person_score = 0; 
-    tie = 0;
-    for (let i = 0; i < 5; i++) {
-        computer = getComputerChoice(); 
-        person = convert_selection_string(); 
-        isinArray = new_list.includes(person)  
-        if (isinArray === false) {
-            alert(`Invalid ${person}`);
+function game(person, scores) {
+    computer = getComputerChoice();  
+    isinArray = new_list.includes(person)  
+    if (isinArray === false) {
+        alert(`Invalid ${person}`);
+    } else {
+        const div = document.createElement('div');
+        if (person === 'Rock' && computer === 'Scissors') { 
+            div.textContent = `You Win! ${person} beats ${computer}`;
+            body.appendChild(div);
+            scores[1]++;
+        } else if (person === 'Scissors' && computer === 'Paper') {
+            div.textContent = `You Win! ${person} beats ${computer}`;
+            body.appendChild(div);
+            scores[1]++;
+        } else if (person === 'Paper' && computer === 'Rock') {
+            div.textContent = `You Win! ${person} beats ${computer}`;
+            body.appendChild(div);
+            scores[1]++;
+        } else if (person === computer) {
+            div.textContent = `Tie`;
+            body.appendChild(div);
+            scores[2]++;
         } else {
-            if (person === 'Rock' && computer === 'Scissors') {
-                console.log(`You Win! ${person} beats ${computer}`);
-                person_score++;
-            } else if (person === 'Scissors' && computer === 'Paper') {
-                console.log(`You Win! ${person} beats ${computer}`);
-                person_score++;
-            } else if (person === 'Paper' && computer === 'Rock') {
-                console.log(`You Win! ${person} beats ${computer}`);
-                person_score++;
-            } else if (person === computer) {
-                console.log('Tie');
-                tie++;
-            } else {
-                console.log(`You Lose! ${computer} beats ${person}`);
-                computer_score++;
-            }
+            div.textContent = `You Lose! ${computer} beats ${person}`;
+            body.appendChild(div);
+            scores[3]++;
         }
+        scores[0]++;
     }
-    winner(computer_score, person_score);
+    return scores;
 }
 
-game();
+
+const scores = [0, 0, 0, 0];
+function log_text (element, scores) {
+    scores = game(element, scores)
+    const div = document.createElement('div');
+    console.log(scores)
+    if (scores[3] === 5) {
+        div.textContent = 'Computer Wins!';
+        body.appendChild(div);
+    } else if (scores[1] === 5) {
+        div.textContent = 'You Win!';
+        body.appendChild(div);
+    }
+}
+
+
+
+
+const btn_1 = document.querySelector('#Rock');
+btn_1.addEventListener('click', () => log_text('Rock', scores));
+
+const btn_2 = document.querySelector('#Paper');
+btn_2.addEventListener('click', () => log_text('Paper', scores));
+
+const btn_3 = document.querySelector('#Scissors');
+btn_3.addEventListener('click', () => log_text('Scissors', scores));
 
 
 
